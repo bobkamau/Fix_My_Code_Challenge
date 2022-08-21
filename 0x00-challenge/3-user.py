e8d537db22b80f9c11e69b81a8e206b361d8a12b
bobkamau <robertmwangi369@gmail.com>
2022-08-21 03:31:50 -0700

    3

---
+++ 0x00-challenge/3-user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ User Model
+ """
+ import hashlib
+ import uuid
+
+
+ class User():
+         """
+             User class:
+                 - id: public string unique (uuid)
+                     - password: private string hash in MD5
+                         """
+
+                             __password = None
+
+                                 def __init__(self):
+                                             """
+                                                     Initialize a new user:
+                                                             - assigned an unique `id`
+                                                                     """
+                                                                             self.id = str(uuid.uuid4())
+
+                                                                                 @property
+                                                                                     def password(self):
